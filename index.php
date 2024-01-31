53c38ee07f6c5420e6d827fd1d30fa35a20cbd0c
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2024-01-31 16:40:14 +0200

    chore: optimize directory

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ Kirby::plugin('bogdancondorachi/table-field', [
   ],
 
   'blueprints' => [
-    'blocks/table' => __DIR__ . '/blueprints/blocks/table.yml'
+    'blocks/table' => __DIR__ . '/src/extensions/blueprints/blocks/table.yml'
   ],
 
   'snippets' => [
-    'blocks/table' => __DIR__ . '/snippets/blocks/table.php'
+    'blocks/table' => __DIR__ . '/src/extensions/snippets/blocks/table.php'
   ],
 
-  'fieldMethods' => require_once __DIR__ . '/lib/methods.php',
+  'fieldMethods' => require_once __DIR__ . '/src/extensions/methods.php',
 
-  'translations' => require_once __DIR__ . '/lib/translations.php'
+  'translations' => require_once __DIR__ . '/src/extensions/translations.php'
 ]);
